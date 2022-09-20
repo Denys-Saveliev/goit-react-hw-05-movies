d051f98e533c49a4b84cefea0709afbae7c2a5aa
Denys-Saveliev <90144527+Denys-Saveliev@users.noreply.github.com>
2022-09-20 18:33:55 +0300

    updates

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,6 @@
-import { lazy, Suspense } from 'react';
+import { lazy } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { Layout } from './Layout/Layout';
-
-import { RotatingLines } from 'react-loader-spinner';
 
 const HomePage = lazy(() =>
   import('../pages/HomePage' /*webpackChunkName: "homePage-view"*/)
@@ -18,31 +16,13 @@ const MovieDetailsPage = lazy(() =>
 
 export const App = () => {
   return (
-    <Suspense
-      fallback={
-        <div
-          style={{
-            position: 'absolute',
-            top: '50%',
-            left: '50%',
-            transform: 'translate(-50%, -50%)',
-          }}
-        >
-          <RotatingLines strokeColor="#eee56b" />
-        </div>
-      }
-    >
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<HomePage />} />
-          <Route path="movies" element={<MoviesPage />}></Route>
-          <Route
-            path="movies/:movieId/*"
-            element={<MovieDetailsPage />}
-          ></Route>
-          <Route path="*" element={<Navigate to="/" />} />
-        </Route>
-      </Routes>
-    </Suspense>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route path="movies" element={<MoviesPage />}></Route>
+        <Route path="movies/:movieId/*" element={<MovieDetailsPage />}></Route>
+        <Route path="*" element={<Navigate to="/" />} />
+      </Route>
+    </Routes>
   );
 };
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,12 +1,31 @@
+import { Suspense } from 'react';
+
 import { Container } from './Layout.styled';
 import { AppBar } from 'components/AppBar/AppBar';
 import { Outlet } from 'react-router-dom';
+
+import { RotatingLines } from 'react-loader-spinner';
 
 export const Layout = () => {
   return (
     <Container>
       <AppBar />
-      <Outlet />
+      <Suspense
+        fallback={
+          <div
+            style={{
+              position: 'absolute',
+              top: '50%',
+              left: '50%',
+              transform: 'translate(-50%, -50%)',
+            }}
+          >
+            <RotatingLines strokeColor="#eee56b" />
+          </div>
+        }
+      >
+        <Outlet />
+      </Suspense>
     </Container>
   );
 };
